64d7a0a6e9a20654e189f7260fff796e7e5fbdb6
Octavio Lara <octavioasapchi@gmail.com>
2023-08-25 21:05:20 -0400

    add basic auth system and sale system

--- src/app.py
+++ src/app.py
@@ -3,13 +3,26 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Car, Sale
+
+import datetime
 #from models import Person
+
+# Hash | Salt
+# palabra = a;slkdkfjaslkdjfh8123h1326554;as[]
+# Salt es una serie de caracteres secretos que uno agrega a una encriptacion para hacerla mas complicada
+
+
+# palabra + salt -> asdfjqwe+;laksdhjf
+# salt en la aplicacion
+
+salt = "asdf9128"
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -44,6 +57,119 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/register', methods=['POST'])
+def register():
+    # email - password
+    data = request.get_json() # Recibes informacion
+    email = data.get("email", None)
+    # NUNCA GUARDAS LA CONTRASEÑA TEXTUAL
+    password = data.get("password", None)
+
+    # Encriptamos la contraseña
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(email=email, password=hashed_password)
+    # REGEX
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify("Usuario registrado con exito!"), 201 
+    except Exception as error:
+        db.session.rollback()
+        return error, 500
+
+# params(ruta dinamica)
+# body -> request.get_json()
+# method -> request.method
+
+# POST -> Para crear y recibir informacion
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email", None)
+    password = data.get("password", None)
+    
+    # existe el usuario actual
+    current_user = User.query.filter_by(email=email).first()
+    if not current_user:
+        return jsonify({"error": "User not found"}), 404
+
+    # Comparamos contraseña
+    passwords_match = check_password_hash(current_user.password, password)
+    if not passwords_match:
+        return jsonify({"error": "Contraseña invalida"}), 401
+    
+    return jsonify("Loggeado con exito")
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    cars = Car.query.all()
+    return jsonify({"cars": [car.serialize() for car in cars]}), 200
+
+@app.route('/car', methods=['POST'])
+def create_car():
+    data = request.get_json()
+    brand = data.get("brand", None)
+    model = data.get("model", None)
+    year = data.get("year", None)
+    price = data.get("price", None)
+    user_id = data.get("user_id", None)
+
+    new_car = Car(brand=brand, model=model, year=year, price=price, user_id=user_id)
+
+    try:
+        db.session.add(new_car)
+        db.session.commit()
+        return jsonify({"car": new_car.serialize()}), 201
+
+    except Exception as error:
+        db.session.rollback()
+        return error, 500
+
+@app.route('/sale/<int:car_id>', methods=['POST'])
+def create_sale(car_id):
+    # -> precio viene del carro
+    # fecha es hoy
+    # commision a partir del precio
+    # vendedor
+    # comprador
+    data = request.get_json()
+    buyer_id = data.get('buyer_id', None)
+
+    # validar si el carro existe
+    current_car = Car.query.get(car_id)
+
+    if not current_car:
+        return jsonify({"error": "Car not found"}), 404
+    
+    buyer_user = User.query.get(buyer_id)
+
+    if not buyer_user:
+        return jsonify({"error": "no autorizado"}), 401
+    
+    current_date = datetime.datetime.now()
+
+    commision = current_car.price * 0.02
+    new_sale = Sale(
+        price=current_car.price,
+        sale_date=current_date,
+        commision=commision,
+        car_id=car_id,
+        seller_id=current_car.user_id,
+        buyer_id=buyer_id
+    )
+    try:
+        db.session.add(new_sale)
+        db.session.commit()
+        return jsonify("Venta realizada con exito"), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(error), 500
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,95 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
+    # cars = db.relationship()
 
+    cars = db.relationship("Car", backref="user", lazy=True)
+
+
+    shopping_carts = db.relationship("ShoppingCart", backref="user", lazy=True)
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Car(db.Model):
+    """
+    Car(Marca, Modelo, Año, Color, Kilometraje, Precio, Descripcion)
+    Relaciones(User, Sales, ShoppingCart)
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(50), unique=False, nullable=False)
+    model = db.Column(db.String(50), unique=False, nullable=False)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+    shopping_carts = db.relationship("ShoppingCart", backref="car", lazy=True)
+    sale = db.relationship("Sale", backref="car")
+
+    def __repr__(self):
+        return f"<Car {self.brand} {self.model} - {self.year}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "brand": self.brand,
+            "model": self.model,
+            "year": self.year,
+            "price": self.price
+        }
+class Sale(db.Model):
+    # Campos
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    sale_date = db.Column(db.Date)
+    commision = db.Column(db.Integer)
+    
+    # Claves foraneas
+    car_id = db.Column(db.Integer, db.ForeignKey("car.id"), nullable=False)
+
+    # 2 relaciones con la misma table
+    # Las claves foraneas
+    # seller_id -> el vendedor
+    seller_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False) 
+    # buyer_id -> que compro
+    buyer_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    # 2 relaciones con la tabla usuario
+    seller = db.relationship("User",  foreign_keys=[seller_id]) #
+    purchaser = db.relationship("User", foreign_keys=[buyer_id]) #
+    def __repr__(self):
+        return f"<Sale {self.price} {self.sale_date}>"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            "sale_date": self.sale_date,
+            "commision": self.commision
+        }
+class ShoppingCart(db.Model):
+    """
+    Relaciones(User, Car)
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey("car.id"), nullable=False)
+    
+    def __repr__(self):
+        return f"<ShoppingCart {self.id}>"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "car_id": self.car_id
         }
